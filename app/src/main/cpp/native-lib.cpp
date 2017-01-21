293468c69f0fcde4e1a6f7d00c49baf90deee1eb
smoon72 <sjm9219@gmail.com>
2017-01-21 02:22:12 -0500

    Camera working, but not openCV

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,6 +6,6 @@ jstring
 Java_com_swamphacks_jssk_takenotes_Main_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Take a picture";
     return env->NewStringUTF(hello.c_str());
 }
--- app/src/main/java/com/swamphacks/jssk/takenotes/Main.java
+++ app/src/main/java/com/swamphacks/jssk/takenotes/Main.java
@@ -1,6 +1,8 @@
 package com.swamphacks.jssk.takenotes;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.TextView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,30 +11,65 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.ImageView;
+import java.io.File;
+import android.content.Intent;
+import android.net.Uri;
 
-public class Main extends AppCompatActivity {
+public class Main extends Activity {
+
+    /*
+
+    For regular camera and also must extends Activity
+
+    Button button;
+    ImageView imageView;
+    static final int CAM_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        button = (Button) findViewById(R.id.button);
+        imageView = (ImageView)findViewById(R.id.image_view);
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v){
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file = getFile();
+                camera_intent.putExtra(MediaStore.ACTION_IMAGE_CAPTURE, Uri.fromFile(file));
+                startActivityForResult(camera_intent, CAM_REQUEST);
             }
         });
-
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
     }
 
+    private File getFile(){
+        File folder = new File("sdcard/camera_app");
+
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+
+        File image_file = new File(folder, "cam_image.jpg");
+        return image_file;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String path = "sdcard/camera_app/cam_image.jpg";
+        imageView.setImageDrawable(Drawable.createFromPath(path));
+
+    }
+
+    */
+
+    /*
+
+    Orginal
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -54,6 +91,7 @@ public class Main extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    */
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
